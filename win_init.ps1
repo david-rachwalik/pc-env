bdd4539c47a9c5a7eddc927d8527af78d017e5a6
David Rachwalik <david.rachwalik@outlook.com>
2024-10-31 15:34:33 -0500

    updated paths to provisioning chocolatey & python

--- win_init.ps1
+++ win_init.ps1
@@ -38,7 +38,7 @@ if ($execution_policy -ne $expected_policy)
 # -------- STAGE 2: Provision Chocolatey --------
 
 Write-Host "Calling 'provision_chocolatey.ps1' from remote..."
-$provision_chocolatey_url = "https://raw.githubusercontent.com/david-rachwalik/pc-env/master/powershell/provision_chocolatey.ps1"
+$provision_chocolatey_url = "https://raw.githubusercontent.com/david-rachwalik/pc-env/master/setup-windows/provision_chocolatey.ps1"
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest
 $provision_chocolatey_script = Invoke-WebRequest $provision_chocolatey_url
 Invoke-Expression $provision_chocolatey_script.Content
@@ -48,7 +48,7 @@ Write-Host "Completed 'provision_chocolatey.ps1' process"
 # -------- STAGE 3: Provision Python --------
 
 Write-Host "Calling 'provision_python.ps1' from remote..."
-$provision_python_url = "https://raw.githubusercontent.com/david-rachwalik/pc-env/master/powershell/provision_python.ps1"
+$provision_python_url = "https://raw.githubusercontent.com/david-rachwalik/pc-env/master/setup-windows/provision_python.ps1"
 $provision_python_script = Invoke-WebRequest $provision_python_url
 Invoke-Expression $provision_python_script.Content
 Write-Host "Completed 'provision_python.ps1' process"
