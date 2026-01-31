a14cbd3d19e02c8f8923a17b49266122b9d7dd6f
David Rachwalik <david.rachwalik@outlook.com>
2026-01-31 11:01:09 +0000

    major fixes to JS (now using Deno), signature decryption, & handling of default format (now always assumed alias will override for audio or others)

--- docker/yt-dlp/entrypoint.sh
+++ docker/yt-dlp/entrypoint.sh
@@ -3,9 +3,12 @@
 # Update required packages
 pip install pip -U
 pip install yt-dlp -U
+pip install yt-dlp-ejs -U
 
 # Define aliases for audio and video downloads
 echo 'alias ytv="yt-dlp"' >>/root/.shrc
+echo 'alias ytvr="yt-dlp -f \"bv*+ba/b\""' >>/root/.shrc # Best video (no resolution limit)
+# Placed audio formats first so `--extract-audio|-x` only attempts to download audio when possible
 echo 'alias yta="yt-dlp -x -f \"ba[abr<=128k]/ba/b[abr<=128k]/b\""' >>/root/.shrc
 
 # Ensure aliases are sourced for interactive shells
