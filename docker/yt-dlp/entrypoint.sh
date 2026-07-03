b7e67b965c8cbedde1dc0e8cda1083281877c0f9
David Rachwalik <david.rachwalik@outlook.com>
2026-07-03 19:41:16 +0000

    migrated last remaining "alpine" image to using "slim" (Debian-based) version; handling dependencies with apt-get instead of apk

--- docker/yt-dlp/entrypoint.sh
+++ docker/yt-dlp/entrypoint.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # Update required packages
-pip install pip -U
-pip install yt-dlp -U
-pip install yt-dlp-ejs -U
+pip install -U --root-user-action=ignore pip
+pip install -U --root-user-action=ignore yt-dlp
+pip install -U --root-user-action=ignore yt-dlp-ejs
 
 # Define aliases for audio and video downloads
-echo 'alias ytv="yt-dlp"' >>/root/.shrc
-echo 'alias ytvr="yt-dlp -f \"bv*+ba/b\""' >>/root/.shrc # Best video (no resolution limit)
-# Placed audio formats first so `--extract-audio|-x` only attempts to download audio when possible
-echo 'alias yta="yt-dlp -x -f \"ba[abr<=128k]/ba/b[abr<=128k]/b\""' >>/root/.shrc
+{
+    echo 'alias ytv="yt-dlp"'
+    echo 'alias ytvr="yt-dlp -f \"bv*+ba/b\""' # Best video (no resolution limit)
+    # Placed audio formats first so `--extract-audio|-x` only attempts to download audio when possible
+    echo 'alias yta="yt-dlp -x -f \"ba[abr<=128k]/ba/b[abr<=128k]/b\""'
+} >> /root/.bashrc
 
-# Ensure aliases are sourced for interactive shells
-echo '[ -f /root/.shrc ] && . /root/.shrc' >/root/.profile
-
-exec /bin/sh -l # Keeps the container running interactively
+exec /bin/bash -l # Keeps the container running interactively
